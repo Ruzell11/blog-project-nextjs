8e02da01717a21c817f9d6b1289e0dc6dd5413a8
Ruzell11 <johnruzell123@gmail.com>
2022-09-02 10:19:20 +0800

    comment-area

--- components/Author.js
+++ components/Author.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const Author = () => {
+const Author = ({author}) => {
   return (
-    <div>
-        <h1>
-        Author
-        </h1>
+    <div className=' bg-black  bg-opacity-20 text-center  mt-20 mb-8 p-12 relative rounded-lg font-serif '>
+      <div className="absolute left-0 right-0 -top-14 mx-auto flex w-full justify-center items-center ">
+      <img src={author.photo.url} alt={author.name} unoptimized
+      height='100px' width='100px'  />
+     
+      </div>
+      <h3 className='text-white my-2 text-xl font-bold'>{author.name}</h3>
+      <p className='text-white text-lg'>{author.bio}</p>
     </div>
   )
 }
--- components/CommentsForm.js
+++ components/CommentsForm.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React , {useState , useEffect , useRef} from 'react'
 
 const CommentsForm = () => {
+  const [error , setError] = useState(false)
+  const [localStorage , setLocalStorage] = useState(null)
+  const [showSucessMessage , setShowSucessMessage ] = useState(false)
+  const commentEl = useRef();
+  const nameEl = useRef();
+  const emailEl = useRef();
+  const storeDataEl = useRef();
+
+
+
   return (
-    <div>CommentsForm</div>
+    <div className='bg-white rounded-md shadow-lg p-8 pb-12 mb-8'>
+      <h3 className=' text-center text-xl mb-8 font-bold border-b pb-4'>Comments Form</h3>
+        <div className='grid grid-cols-1 gap-4 mb-2'>
+          <textarea 
+          ref={commentEl}
+          name='comment' 
+          className=' bg-gray-700/20 rounded-md p-5  focus:outline-offset-2 outline-1 resize-none' placeholder='Type your comments...'>
+          </textarea>
+          <div className='grid grid-cols-1 gap-4 '>
+          </div>
+          <div className='grid grid-cols-1 gap-4 mb-2  mx-auto w-100'>
+            <button className='btn rounded-full py-2 px-2 bg-blue-700  text-white font-semibold '>Post Message</button>
+          </div>
+
+        </div>
+    </div>
   )
 }
 
